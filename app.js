108cd2f5d8a978c650b978c8cdcb907fb45c08df
zgtcgd <132292985+zgtcgd@users.noreply.github.com>
2025-08-01 01:36:52 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,53 +1,35 @@
-const port = process.env.PORT || process.env.SERVER_PORT || 3000;
+const port = process.env.PORT || 3000;
 const FILE_PATH = process.env.FILE_PATH || '/tmp';
+const UUID = process.env.UUID;
 const http = require('http');
-const fs = require('fs');
+const fs = require("fs");
+const path = require("path");
 const { spawn } = require('child_process');
-const openhttp = process.env.openhttp || '0'; // 0 or 1
 
-const startScriptPath = `./start.sh`;
-fs.chmodSync(startScriptPath, 0o755);
-const startScript = spawn(startScriptPath, [], {
-    env: {
-        ...process.env,
-        openhttp: openhttp
+const subfilePath = path.join(FILE_PATH, "log.txt");
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200);
+        res.end('hello world');
+    } else if (req.url === `/${UUID}`) {
+        fs.readFile(subfilePath, (err, data) => {
+            if (err) {
+                return res.status(500).send('Error reading file');
+            }
+            res.type("txt").send(data);
+        });
+    } else {
+        res.writeHead(404);
+        res.end('Not found');
     }
 });
-startScript.stdout.on('data', (data) => {
-    console.log(`${data}`);
-});
-startScript.stderr.on('data', (data) => {
-    console.error(`${data}`);
-});
-startScript.on('error', (error) => {
-    console.error(`boot error: ${error}`);
-    process.exit(1);
+
+const startScriptPath = `./start.sh`;
+const childProcess = spawn(startScriptPath, [], {
+    detached: false,
+    stdio: 'inherit',
 });
 
-if (openhttp === '1') {
-    const subFilePath = FILE_PATH + '/log.txt';
-    const server = http.createServer((req, res) => {
-        if (req.url === '/') {
-            res.writeHead(200);
-            res.end('hello world');
-        } else if (req.url === '/sub') {
-            fs.readFile(subFilePath, 'utf8', (error, data) => {
-                if (error) {
-                    res.writeHead(500);
-                    res.end('Error reading file');
-                } else {
-                    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-                    res.end(data);
-                }
-            });
-        } else {
-            res.writeHead(404);
-            res.end('Not found');
-        }
-    });
-    server.listen(port, () => {
-        console.log(`server is listening on port ${port}`);
-    });
-} else if (openhttp === '0') {
+server.listen(port, () => {
     console.log(`server is listening on port ${port}`);
-}
+});
